31434aefd836a7c62e51293e532e06a398a621b6
RandomEyeMotion <jay.180106@gmail.com>
2026-01-19 11:09:11 +0000

    added border

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -22,12 +22,18 @@ return {
 			documentation = {
 				auto_show = true,
 				auto_show_delay_ms = 500,
+				window = {
+					border = "rounded",
+				},
 			},
 			list = {
 				selection = {
 					preselect = false,
 					auto_insert = false,
 				},
+			},
+			menu = {
+				border = "rounded",
 			},
 		},
 		signature = { enabled = true },
