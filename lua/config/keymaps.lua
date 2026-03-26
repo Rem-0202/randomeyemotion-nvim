0ff7856e190a9ae1d777d88cbeda6b93aa52b194
RandomEyeMotion <jay.180106@gmail.com>
2026-03-26 15:48:12 +0000

    conform + mason bridge

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,127 +1,130 @@
 vim.g.mapleader = " "
 vim.api.nvim_create_autocmd("User", {
-	pattern = "VeryLazy",
-	callback = function()
-		local map = require("snacks").keymap.set
-		-- Normal mode: splits --
-		map("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
-		map("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
-		map("n", "<C-k>", "<C-w>k", { desc = "Move to above split" })
-		map("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
-		map("n", "<C-S-h>", "<C-w><", { desc = "Increase Width" })
-		map("n", "<C-S-l>", "<C-w>>", { desc = "Decrease Width" })
-		map("n", "<C-S-k>", "<C-w>+", { desc = "Increase Height" })
-		map("n", "<C-S-j>", "<C-w>-", { desc = "Decrease Height" })
+    pattern = "VeryLazy",
+    callback = function()
+        local map = require("snacks").keymap.set
+        -- Normal mode: splits --
+        map("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+        map("n", "<C-j>", "<C-w>j", { desc = "Move to below split" })
+        map("n", "<C-k>", "<C-w>k", { desc = "Move to above split" })
+        map("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+        map("n", "<C-S-h>", "<C-w><", { desc = "Increase Width" })
+        map("n", "<C-S-l>", "<C-w>>", { desc = "Decrease Width" })
+        map("n", "<C-S-k>", "<C-w>+", { desc = "Increase Height" })
+        map("n", "<C-S-j>", "<C-w>-", { desc = "Decrease Height" })
 
-		-- Insert mode: splits --
-		map("i", "<C-h>", "<Esc><C-w>h", { desc = "Move to left split" })
-		map("i", "<C-j>", "<Esc><C-w>j", { desc = "Move to below split" })
-		map("i", "<C-k>", "<Esc><C-w>k", { desc = "Move to above split" })
-		map("i", "<C-l>", "<Esc><C-w>l", { desc = "Move to right split" })
+        -- Insert mode: splits --
+        map("i", "<C-h>", "<Esc><C-w>h", { desc = "Move to left split" })
+        map("i", "<C-j>", "<Esc><C-w>j", { desc = "Move to below split" })
+        map("i", "<C-k>", "<Esc><C-w>k", { desc = "Move to above split" })
+        map("i", "<C-l>", "<Esc><C-w>l", { desc = "Move to right split" })
 
-		-- Clear search highlight --
-		map("n", "<Leader><CR>", "<cmd>noh<CR>", { silent = true }, { desc = "Clear search highlight" })
+        -- Clear search highlight --
+        map("n", "<Leader><CR>", "<cmd>noh<CR>", { silent = true }, { desc = "Clear search highlight" })
 
-		-- File tree --
-		map("n", "<leader>n", "<cmd>NvimTreeToggle<CR>", { silent = true }, { desc = "Toggle file tree" })
+        -- File tree --
+        map("n", "<leader>n", "<cmd>NvimTreeToggle<CR>", { silent = true }, { desc = "Toggle file tree" })
 
-		-- Basic file ops --
-		map("n", "<leader>w", "<cmd>w<CR>", { silent = true }, { desc = "Save file" })
-		map("n", "<leader>q", "<cmd>q<CR>", { silent = true }, { desc = "Quit file" })
-		map("n", "<leader>Q", "<cmd>q!<CR>", { silent = true }, { desc = "Quit without saving" })
+        -- Basic file ops --
+        map("n", "<leader>w", "<cmd>w<CR>", { silent = true }, { desc = "Save file" })
+        map("n", "<leader>q", "<cmd>q<CR>", { silent = true }, { desc = "Quit file" })
+        map("n", "<leader>Q", "<cmd>q!<CR>", { silent = true }, { desc = "Quit without saving" })
 
-		-- Increment / Decrement numbers --
-		map("n", "<leader>+", "<C-a>", { silent = true }, { desc = "Increment number" })
-		map("n", "<leader>_", "<C-x>", { silent = true }, { desc = "Decrement number" })
+        -- Increment / Decrement numbers --
+        map("n", "<leader>+", "<C-a>", { silent = true }, { desc = "Increment number" })
+        map("n", "<leader>_", "<C-x>", { silent = true }, { desc = "Decrement number" })
 
-		-- Prevent delete from yanking --
-		map("n", "d", '"_d', { silent = true })
-		map("v", "d", '"_d', { silent = true })
-		map("n", "c", '"_c', { silent = true })
-		map("v", "c", '"_c', { silent = true })
-		map("n", "x", '"_x', { silent = true })
-		map("v", "x", '"_x', { silent = true })
-		map("n", "<leader>d", "d", { silent = true }, { desc = "Delete with yank" })
-		map("v", "<leader>d", "d", { silent = true }, { desc = "Delete with yank" })
+        -- Prevent delete from yanking --
+        map("n", "d", '"_d', { silent = true })
+        map("v", "d", '"_d', { silent = true })
+        map("n", "c", '"_c', { silent = true })
+        map("v", "c", '"_c', { silent = true })
+        map("n", "x", '"_x', { silent = true })
+        map("v", "x", '"_x', { silent = true })
+        map("n", "<leader>d", "d", { silent = true }, { desc = "Delete with yank" })
+        map("v", "<leader>d", "d", { silent = true }, { desc = "Delete with yank" })
 
-		-- Scrolling --
-		map("n", "<PageDown>", "<C-d>", { silent = true })
-		map("n", "<PageUp>", "<C-u>", { silent = true })
+        -- Scrolling --
+        map("n", "<PageDown>", "<C-d>", { silent = true })
+        map("n", "<PageUp>", "<C-u>", { silent = true })
 
-		-- Movement --
-		map("n", "j", function()
-			return vim.v.count == 0 and "gj" or "j"
-		end, { expr = true })
+        -- Movement --
+        map("n", "j", function()
+            return vim.v.count == 0 and "gj" or "j"
+        end, { expr = true })
 
-		map("n", "k", function()
-			return vim.v.count == 0 and "gk" or "k"
-		end, { expr = true })
+        map("n", "k", function()
+            return vim.v.count == 0 and "gk" or "k"
+        end, { expr = true })
 
-		-- Tabs --
-		map("n", "<M-l>", "<cmd>BufferLineCycleNext<CR>", { silent = true }, { desc = "Next buffer" })
-		map("n", "<M-h>", "<cmd>BufferLineCyclePrev<CR>", { silent = true }, { desc = "Previous buffer" })
-		map("i", "<M-l>", "<ESC><cmd>bn<CR>", { silent = true }, { desc = "Next buffer" })
-		map("i", "<M-h>", "<ESC><cmd>bp<CR>", { silent = true }, { desc = "Previous buffer" })
-		map("n", "<leader><C-w>", "<cmd>bd<CR>", { silent = true }, { desc = "Close buffer" })
+        -- Tabs --
+        map("n", "<M-l>", "<cmd>BufferLineCycleNext<CR>", { silent = true }, { desc = "Next buffer" })
+        map("n", "<M-h>", "<cmd>BufferLineCyclePrev<CR>", { silent = true }, { desc = "Previous buffer" })
+        map("i", "<M-l>", "<ESC><cmd>bn<CR>", { silent = true }, { desc = "Next buffer" })
+        map("i", "<M-h>", "<ESC><cmd>bp<CR>", { silent = true }, { desc = "Previous buffer" })
+        map("n", "<leader><C-w>", "<cmd>bd<CR>", { silent = true }, { desc = "Close buffer" })
 
-		-- Latex --
-		map({ "n", "v", "i" }, "<F5>", function()
-			require("knap").toggle_autopreviewing()
-		end, { desc = "Knap autopreview", ft = { "tex" } })
-		map({ "n", "v", "i" }, "<F6>", function()
-			require("knap").close_viewer()
-		end, { desc = "Close preview", ft = { "tex" } })
+        -- Latex --
+        map({ "n", "v", "i" }, "<F5>", function()
+            require("knap").toggle_autopreviewing()
+        end, { desc = "Knap autopreview", ft = { "tex" } })
+        map({ "n", "v", "i" }, "<F6>", function()
+            require("knap").close_viewer()
+        end, { desc = "Close preview", ft = { "tex" } })
 
-		-- Markdown --
-		map("n", "<F5>", function()
-			require("render-markdown").toggle()
-		end, { desc = "Toggle RenderMarkdown", ft = { "markdown" } })
+        -- Markdown --
+        map("n", "<F5>", function()
+            require("render-markdown").toggle()
+        end, { desc = "Toggle RenderMarkdown", ft = { "markdown" } })
 
-		-- Make
-		map("n", "<F5>", "<Cmd>MakeitOpen<CR>", { desc = "Run Make" })
+        -- Make
+        map("n", "<F5>", "<Cmd>MakeitOpen<CR>", { desc = "Run Make" })
 
-		-- LSP --
+        -- LSP --
 
-		map("n", "grd", vim.lsp.buf.definition, { silent = true }, { desc = "Goes to the definition" })
-		map("n", "<leader>h", vim.lsp.buf.hover, { silent = true }, { desc = "Show help" })
-		map("n", "<leader>rn", vim.lsp.buf.rename, { silent = true }, { desc = "Renames a symbol" })
-		map("n", "<leader>fm", vim.lsp.buf.format, { silent = true }, { desc = "Format file" })
-		map("n", "<leader>df", function()
-			vim.diagnostic.open_float(nil, { focus = false })
-		end, { desc = "Open diagnostic float" })
+        map("n", "grd", vim.lsp.buf.definition, { silent = true }, { desc = "Goes to the definition" })
+        map("n", "<leader>h", vim.lsp.buf.hover, { silent = true }, { desc = "Show help" })
+        map("n", "<leader>rn", vim.lsp.buf.rename, { silent = true }, { desc = "Renames a symbol" })
+        map("n", "<leader>fm", function()
+            require("conform").format()
+        end, { silent = true }, { desc = "Format file" })
 
-		-- Quickfix list --
-		map("n", "<leader>cn", "<Cmd>cn<CR>", { silent = true }, { desc = "Next quick fix list item" })
-		map("n", "<leader>cp", "<Cmd>cp<CR>", { silent = true }, { desc = "Previous quick fix list item" })
-		map("n", "<leader>cq", "<Cmd>cclose<CR>", { silent = true }, { desc = "Close quick fix list" })
-		map("n", "q", "<Cmd>cclose<CR>", { silent = true }, { ft = { "qf" }, desc = "Close quick fix list" })
+        map("n", "<leader>df", function()
+            vim.diagnostic.open_float(nil, { focus = false })
+        end, { desc = "Open diagnostic float" })
 
-		-- Picker --
-		map("n", "<leader>pp", "<Cmd>lua Snacks.picker()<CR>", { silent = true }, { desc = "Open picker" })
-		map("n", "<leader>pl", "<Cmd>lua Snacks.picker.lines()<CR>", { silent = true }, { desc = "Lines search" })
-		map("n", "<leader>pf", "<Cmd>lua Snacks.picker.files()<CR>", { silent = true }, { desc = "Files Searc" })
-		map("n", "<leader>pg", "<Cmd>lua Snacks.picker.grep()<CR>", { silent = true }, { desc = "Live Grep" })
-		map("n", "<leader>pd", "<Cmd>lua Snacks.picker.diagnostics()<CR>", { silent = true }, { desc = "Live Grep" })
-		map(
-			"n",
-			"<leader>pb",
-			"<Cmd>lua Snacks.picker.grep_buffers()<CR>",
-			{ silent = true },
-			{ desc = "Buffers Grep" }
-		)
-		map(
-			"n",
-			"<M-q>",
-			"<Cmd>lua Snacks.picker.actions.qflist()<CR>",
-			{ silent = true },
-			{ desc = "Send to quickfix list" }
-		)
+        -- Quickfix list --
+        map("n", "<leader>cn", "<Cmd>cn<CR>", { silent = true }, { desc = "Next quick fix list item" })
+        map("n", "<leader>cp", "<Cmd>cp<CR>", { silent = true }, { desc = "Previous quick fix list item" })
+        map("n", "<leader>cq", "<Cmd>cclose<CR>", { silent = true }, { desc = "Close quick fix list" })
+        map("n", "q", "<Cmd>cclose<CR>", { silent = true }, { ft = { "qf" }, desc = "Close quick fix list" })
 
-		-- Outline --
-		map("n", "<leader>ol", "<Cmd>Outline<CR>", { silent = true }, { desc = "Toggle Outline" })
+        -- Picker --
+        map("n", "<leader>pp", "<Cmd>lua Snacks.picker()<CR>", { silent = true }, { desc = "Open picker" })
+        map("n", "<leader>pl", "<Cmd>lua Snacks.picker.lines()<CR>", { silent = true }, { desc = "Lines search" })
+        map("n", "<leader>pf", "<Cmd>lua Snacks.picker.files()<CR>", { silent = true }, { desc = "Files Searc" })
+        map("n", "<leader>pg", "<Cmd>lua Snacks.picker.grep()<CR>", { silent = true }, { desc = "Live Grep" })
+        map("n", "<leader>pd", "<Cmd>lua Snacks.picker.diagnostics()<CR>", { silent = true }, { desc = "Live Grep" })
+        map(
+            "n",
+            "<leader>pb",
+            "<Cmd>lua Snacks.picker.grep_buffers()<CR>",
+            { silent = true },
+            { desc = "Buffers Grep" }
+        )
+        map(
+            "n",
+            "<M-q>",
+            "<Cmd>lua Snacks.picker.actions.qflist()<CR>",
+            { silent = true },
+            { desc = "Send to quickfix list" }
+        )
 
-		-- Terminal --
-		map("n", "<leader>t", "<Cmd>lua Snacks.terminal()<CR>", { silent = true }, { desc = "Toggle Snacks Terminal" })
-		map("t", "<C-q>", "<Cmd>lua Snacks.terminal()<CR>", { silent = true }, { desc = "Escape terminal" })
-	end,
+        -- Outline --
+        map("n", "<leader>ol", "<Cmd>Outline<CR>", { silent = true }, { desc = "Toggle Outline" })
+
+        -- Terminal --
+        map("n", "<leader>t", "<Cmd>lua Snacks.terminal()<CR>", { silent = true }, { desc = "Toggle Snacks Terminal" })
+        map("t", "<C-q>", "<Cmd>lua Snacks.terminal()<CR>", { silent = true }, { desc = "Escape terminal" })
+    end,
 })
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,31 +1,67 @@
--- plugins/lsp.lua
 return {
-	{ "mason-org/mason.nvim", opts = {} },
+	-- LSP + Mason
 	{
-		"SmiteshP/nvim-navic",
-		config = function()
-			require("nvim-navic").setup({
-				lsp = {
-					auto_attach = true,
-				},
-			})
-		end,
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
+		"mason-org/mason.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mason-org/mason-lspconfig.nvim",
+		},
 		config = function()
 			require("mason").setup()
-			require("mason-lspconfig").setup({ auto_setup = true })
 
-			-- Optional global default config for all servers
-			vim.lsp.config("*", {
-				root_markers = { ".git", "src", "Makefile" },
-				capabilities = vim.lsp.protocol.make_client_capabilities(),
-				on_attach = function(client, bufnr)
-					require("nvim-navic").attach(client, bufnr)
+			require("mason-lspconfig").setup({
+				automatic_installation = true,
+			})
+
+			-- Attach navic (if installed) when an LSP attaches
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if not client then
+						return
+					end
+
+					-- only attach navic if the server supports symbols
+					if client.server_capabilities and client.server_capabilities.documentSymbolProvider then
+						local ok, navic = pcall(require, "nvim-navic")
+						if ok then
+							navic.attach(client, args.buf)
+						end
+					end
 				end,
 			})
 		end,
+	},
+
+	-- Conform + mason-bridge (formatters)
+	{
+		"stevearc/conform.nvim",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"frostplexx/mason-bridge.nvim",
+		},
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("mason-bridge").setup({})
+
+			local conform = require("conform")
+			conform.setup({}) -- start empty
+
+			-- 2) Defer applying formatters_by_ft until mason-bridge has populated its cache
+			vim.schedule(function()
+				local fmts = require("mason-bridge").get_formatters()
+				-- apply mapping after it's available
+				conform.setup({ formatters_by_ft = fmts })
+			end)
+		end,
+	},
+
+	-- Navic UI (optional)
+	{
+		"SmiteshP/nvim-navic",
+		dependencies = { "neovim/nvim-lspconfig" },
+		opts = {
+			lsp = { auto_attach = true },
+		},
 	},
 }
